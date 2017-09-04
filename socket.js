d6abba50245d97961c5357c0237d6dd9cfdc4a0c
Carlos E López <hi.oriondev@gmail.com>
2017-09-03 18:59:54 -0600

    z4

--- socket.js
+++ socket.js
@@ -1,21 +1,4 @@
-/**
- * Copyright (c) 2015, Blocshop s.r.o.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the Blocshop s.r.o.. The name of the
- * Blocshop s.r.o. may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
- */
-
-
+cordova.define("cz.blocshop.socketsforcordova.Socket", function(require, exports, module) {
 var exec = require('cordova/exec');
 
 var SOCKET_EVENT = "SOCKET_EVENT";
@@ -88,6 +71,25 @@ Socket.prototype.open = function (host, port, success, error) {
         "open",
         [ this.socketKey, host, port ]);
 };
+
+
+Socket.prototype.s0cread = function (success, error) {
+
+    success = success || function() { };
+    error = error || function() { };
+
+    if (!this._ensureState(Socket.State.OPENED, error)) {
+        return;
+    }
+
+    exec(
+        success,
+        error,
+        CORDOVA_SERVICE_NAME,
+        "s0cread",
+        [ this.socketKey ]);
+};
+
 
 Socket.prototype.write = function (data, success, error) {
 
@@ -211,3 +213,5 @@ if (navigator.userAgent.match(/iemobile/i)) {
 }
 
 module.exports = Socket;
+
+});
--- src/android/src/cz/blocshop/socketsforcordova/SocketPlugin.java
+++ src/android/src/cz/blocshop/socketsforcordova/SocketPlugin.java
@@ -1,20 +1,3 @@
-/**
- * Copyright (c) 2015, Blocshop s.r.o.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the Blocshop s.r.o.. The name of the
- * Blocshop s.r.o. may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
- */
-
 package cz.blocshop.socketsforcordova;
 
 import java.io.IOException;
@@ -33,6 +16,8 @@ import org.json.JSONObject;
 import android.annotation.SuppressLint;
 
 public class SocketPlugin extends CordovaPlugin {
+   
+  String result = "";
 	
 	Map<String, SocketAdapter> socketAdapters = new HashMap<String, SocketAdapter>(); 
 	
@@ -49,11 +34,18 @@ public class SocketPlugin extends CordovaPlugin {
 			this.close(args, callbackContext);
 		} else if (action.equals("setOptions")) {
 			this.setOptions(args, callbackContext);
+		} else if (action.equals("s0cread")) {  
+      this.s0cread(args, callbackContext);
 		} else {
 			callbackContext.error(String.format("SocketPlugin - invalid action:", action));
 			return false;
 		}
 		return true;
+	}
+	
+	private void s0cread(CordovaArgs args, CallbackContext callbackContext) throws JSONException {
+		String socketKey = args.getString(0);
+	  callbackContext.error( this.result );
 	}
 	
 	private void open(CordovaArgs args, CallbackContext callbackContext) throws JSONException {
@@ -156,7 +148,8 @@ public class SocketPlugin extends CordovaPlugin {
 	}
 	
 	private void dispatchEvent(JSONObject jsonEventObject) {
-		this.webView.sendJavascript("console.log('testing')");
+      this.result += jsonEventObject.toString();
+      this.webView.sendJavascript(String.format("console.log(%s)", jsonEventObject.toString()));	
 	}	
 	
 	private class CloseEventHandler implements Consumer<Boolean> {
